073cd721b60f892c2183c652e9a21beb1144bc2e
Nguyễn Kim Chung <133209950+CimChun@users.noreply.github.com>
2024-10-06 21:43:39 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://blooom.onrender.com",
+    origin: process.env.CLIENT_BASE_URL,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
